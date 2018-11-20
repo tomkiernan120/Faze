35ab9320f6efa3e267fe2cfba3536524b7f483b6
tmkiernan <tkiernan120@gmail.com>
2018-11-20 19:34:40 +0000

    wip

--- src/Faze.js
+++ src/Faze.js
@@ -7,13 +7,17 @@
     root.Faze = factory(root);
   }
 })(
-  typeof global !== "undefined" ? global : this.window || this.global,
+  typeof global !== "undefined" ? global : window || this.global,
   function (root) {
     "use strict";
 
     //
     // Variables
     //
+    var identifier,
+    attributes,
+    psuedos,
+    rootFaze;
 
     var Faze = {}; // Object for public APIs
     var supports = !!document.querySelector && !!root.addEventListener; // Feature test
@@ -179,29 +183,56 @@
      * @public
      * @param {Object} options User settings
      */
-    Faze.init = function (options) {
-      // feature test
-      if (!supports) return;
+    Faze.fn = Faze.prototype = {
 
-      // Destroy any existing initializations
-      Faze.destroy();
+      constructor: Faze,
 
-      // Merge user options with defaults
-      settings = this.merge(defaults, options || {});
+      init: function( selector, context, rootFaze ){
+        // feature test
+        if (!supports) return;
+
+        if( !selector ){
+          return this;
+        }
+
+        if( selector.nodeType ){
+          this.context = this[0] = selector;
+          this.length = 1;
+          return this;
+        }
 
 
-      // Add class to HTML element to activate conditional CSS
-      // TODO: Worry about class List
-      // document.documentElement.classList.add(settings.initClass);
+        if( typeof selector === "string" ){
+         // handle html  
+        }
+        else if( !context || context.Faze ){
+          return ( context || rootFaze.find( selector ) );
+        }
+        else {
+          // passing back context to find selector ie. F( ".text", document );
+          return this.constructor( context ).find( selector );
+        }
+  
+        // Destroy any existing initializations
+        Faze.destroy();
+  
+        // Merge user options with defaults
+        settings = this.merge(defaults, options || {});
+  
+  
+        // Add class to HTML element to activate conditional CSS
+        // TODO: Worry about class List
+        // document.documentElement.classList.add(settings.initClass);
+  
+        // @todo Do stuff...
+  
+        // Listen for click events
+        document.addEventListener("click", eventHandler, false);
 
-      // @todo Do stuff...
+        window.Faze = window.F = Faze;
+    
+        return Faze;
+    },
 
-      // Listen for click events
-      document.addEventListener("click", eventHandler, false);
-    };
-
-    window.Faze = window.F = Faze;
-
-    return Faze;
   }
 );
